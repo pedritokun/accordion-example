da020f0cebc07d3728e8a95ecb98970bfb8ff653
Pedro Portocarrero Herencia <pportocarrero@semana.local>
2016-05-11 10:31:24 -0500

    custom functionality

--- accordion.js
+++ accordion.js
@@ -1,24 +1,25 @@
-jQuery(document).ready(function() {
-	function close_accordion_section() {
-		jQuery('.accordion .accordion-section-title').removeClass('active');
-		jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+jQuery(document).ready(function($) {
+	function close_accordion_section(currentAttrValue) {
+		$('.accordion .accordion-section-title[href='+ currentAttrValue +']').removeClass('active');
+        $('.accordion .accordion-section-title[href='+ currentAttrValue +']').next().slideUp(300).removeClass('open');
 	}
 
-	jQuery('.accordion-section-title').click(function(e) {
+	$('#accordion-1').prev().addClass('active');
+    $('#accordion-1').css('display','block');
+
+	$('.accordion-section-title').click(function(e) {
 		// Grab current anchor value
-		var currentAttrValue = jQuery(this).attr('href');
+		var currentAttrValue = $(this).attr('href');
 
-		if(jQuery(e.target).is('.active')) {
-			close_accordion_section();
+		if($(e.target).is('.active')) {
+			close_accordion_section(currentAttrValue);
 		}else {
-			close_accordion_section();
-
 			// Add active class to section title
-			jQuery(this).addClass('active');
+			$(this).addClass('active');
 			// Open up the hidden content panel
-			jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+			$('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
 		}
 
 		e.preventDefault();
 	});
-});
+})(jQuery);
